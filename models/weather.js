60dbeaaa79d9402b56a297ec439f7d87d31f46b0
Farhad Ahmed Sagar <farhad.cfc@gmail.com>
2016-02-02 21:34:29 -0500

    New Features and style change

--- models/weather.js
+++ models/weather.js
@@ -3,7 +3,7 @@ var request = require('request');
 //exports the function whenever its called from other files!!
 module.exports = function (location, callback) {
     var encodedLocation = encodeURIComponent(location);
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q='+encodedLocation+'&appid=44db6a862fba0b067b1930da0d769e98&units=imperial';
+    var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+encodedLocation+'&mode=json&units=imperial&cnt=7&appid=44db6a862fba0b067b1930da0d769e98';
     request({
         url: url,
         json: true
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var weather = require('../models/weather.js');
 var location = require('../models/location.js');
-var city, temp, forecast;
+var city, country, description, wind, dayTemp, nightTemp, eveningTemp, morningTemp, maxTemp, minTemp;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -17,14 +17,37 @@ router.post('/weather-post', function (req, res, next) {
     //callback functions to fetch weather info
     weather(req.body.cityName, function (currentWeather) {
         try {
-            city = currentWeather.name;
-            temp = currentWeather.main.temp;
-            forecast = currentWeather.weather[0].main;
-            res.render('weather-main', { title: 'Temperature', city: city, temp: temp});
+            city = currentWeather.city.name;
+            country = currentWeather.city.country;
+            wind = currentWeather.list[0].speed;
+            description = currentWeather.list[0].weather[0].description;
+
+            //get temperatures
+            dayTemp = currentWeather.list[0].temp.day;
+            nightTemp = currentWeather.list[0].temp.night;
+            eveningTemp = currentWeather.list[0].temp.eve;
+            morningTemp = currentWeather.list[0].temp.morn;
+            maxTemp = currentWeather.list[0].temp.max;
+            minTemp = currentWeather.list[0].temp.min;
+
+            res.render('weather-main', {
+                title: 'Details',
+                city: city,
+                country: country,
+                description: description,
+                wind: wind,
+                dayTemp: dayTemp,
+                nightTemp: nightTemp,
+                eveningTemp: eveningTemp,
+                morningTemp: morningTemp,
+                maxTemp: maxTemp,
+                minTemp: minTemp
+            });
         } catch (err) {
             console.log(err);
             res.redirect('/');
         }
+
     });
 });
 

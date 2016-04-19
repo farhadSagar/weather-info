d5ed52c5519cfa6897ab97eec502ea10ef4e9df2
Farhad Ahmed Sagar <farhad.cfc@gmail.com>
2016-04-19 16:38:25 -0400

    Added API Key for OpenWeatherMap

--- models/weather.js
+++ models/weather.js
@@ -3,7 +3,7 @@ var request = require('request');
 //exports the function whenever its called from other files!!
 module.exports = function (location, callback) {
     var encodedLocation = encodeURIComponent(location);
-    var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+encodedLocation+'&mode=json&units=imperial&cnt=7&appid=44db6a862fba0b067b1930da0d769e98';
+    var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+encodedLocation+'&mode=json&units=imperial&cnt=7&appid=aedb5948152d86c99c9976709adee2fc';
     request({
         url: url,
         json: true
